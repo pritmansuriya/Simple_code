2c24e05bf0c9dcdbef9f728da62e5582d5671a70
Prit Mansuriya <pritmansuriya15@gmail.com>
2025-08-22 19:07:07 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+
+printf("Enter first number: ");
+scanf("%d", &num1);
+
+printf("Enter second number: ");
+scanf("%d", &num2);
+
+sum = num1 + num2;
+printf("Sum = %d\n", sum);
+
+    return 0;
+
+
